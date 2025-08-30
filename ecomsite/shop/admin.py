dec22f7f747c57dc28bef2f8528911484833d400
Sahil-coder50 <sahilsheoran24@gmail.com>
2025-08-30 18:17:59 +0530

    Made Basic Index View

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import Products
 
 # Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display=['id','title','price','discount_price','category','description','image']
+    list_display_links=['title']
+    search_fields=['title']
+
+admin.site.register(Products,ProductsAdmin)
+
